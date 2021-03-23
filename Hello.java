83aa88c5bad437fc6f673bd520a36febcda50629
Benz <Benzeman097-nafazbenzema@gmail.com>
2021-03-23 14:57:14 +0530

    Hello Jenkins Demo

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package com.benz.jenkins.demo;
+
+public class Hello{
+
+    public static void main(String[] args)
+	{
+	   for(int i=0;i<10;i++)
+	      System.out.println("Hello Kelly Brook "+(i+1));
+	}
+}
