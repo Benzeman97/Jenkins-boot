1e2da01a06da8067789c123f2aa39b9eca945086
Benz <Benzeman097-nafazbenzema@gmail.com>
2021-03-23 15:44:51 +0530

    Hello app updated

--- Hello.java
+++ Hello.java
@@ -5,6 +5,6 @@ public class Hello{
     public static void main(String[] args)
 	{
 	   for(int i=0;i<10;i++)
-	      System.out.println("Hello Kelly Brook "+(i+1));
+	      System.out.println("Hello Doto Kama "+(i+1));
 	}
 }
