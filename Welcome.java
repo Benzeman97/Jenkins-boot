86cec4c4ce9353c89460fdb830d55a18b792f8a6
Benz <Benzeman097-nafazbenzema@gmail.com>
2021-03-29 14:42:26 +0530

    Welcome file is added

---
+++ Welcome.java
@@ -0,0 +1,18 @@
+package com.benz.welcome;
+
+public class Welcome{
+
+  public static void main(String... args)
+  {
+       Welcome welcome=new Welcome();
+        welcome.show();
+  }
+
+
+private void show()
+{
+     for(int i=1;i<=5;i++)
+           System.out.println("Welcome Benzema");
+}
+
+}
